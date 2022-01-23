8452ba4c378672fac5ba8c4661e7265c29d871a2
GeOnE <emedarias@gmail.com>
2022-01-23 18:49:56 -0300

    actualizadas funciones a npm

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-require ('process');
+const {config} = require('dotenv');
 
 function main()
 	{
+		config();
 		const secreto = process.env.SECRETO_UNO;
 		console.log("Funcionando! y el secreto es: "+secreto);
 	}
